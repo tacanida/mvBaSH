7177cd985a8449cff27fd4401a26ac7a6d8dffd9
Travis Canida <travis@MStudio.local>
2023-07-09 10:54:26 -0400

    Increase sampling speed and fix some errors

--- R/MBIVS.R
+++ R/MBIVS.R
@@ -1,15 +1,63 @@
 #This is the primary function
 #' @import Rcpp
 
-sourceCpp("/src/matfuns.cpp")
+sourceCpp("src/matfuns.cpp")
+
+# f <- function(X, Y, group, A, burnin = 2500, niter = 5000) {
+#   pi0 <- numeric(burnin+niter); pi0[1] <- rbeta(1,1,1)
+#   pi1 <- matrix(NA, nrow = burnin+niter, ncol = length(group)); pi1[1,] <- rbeta(n = length(group), 1, 1)
+#   pi2 <- numeric(burnin+niter); pi2[1] <- rbeta(1,1,1)
+#   alpha <- matrix(NA, nrow = burnin+niter, ncol = length(unique(group))); alpha[1,] <- rbinom(n = length(unique(group)), size = 1, prob = pi0[1])
+#   gamma <- matrix(NA, nrow = burnin+niter, ncol = length(group)); gamma[1,] <- rbinom(n = length(group), size = 1, prob = pi1[1,])
+#   omega <- matrix(NA, nrow = burnin+niter, ncol = ncol(X) * ncol(Y)); omega[1,] <- rbinom(n = ncol(X) * ncol(Y), size = 1, prob = pi2[1])
+#   chi <- matrix(NA, nrow = burnin+niter, ncol = length(group)); chi[1,] <- rnorm(n = length(group))
+#   a <- matrix(NA, nrow = burnin + niter, ncol = 2); a[1,] <- rnorm(n = 2)
+#   Z <- array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
+#   Z[,,1] <- 0
+#   Z[which(A==1),,1] <- 1
+#   bsigma <- numeric(burnin+niter); bsigma[1] <- runif(1, 0, 1)
+#   b <- array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
+#   b[,,1] <- solve(t(X) %*% X) %*% t(X) %*% Y #least squares starting point
+#   beta <-  array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
+#   beta[,,1] <- Z[,,1] * b[,,1]
+#   Sigma <- array(NA, dim = c(ncol(Y), ncol(Y), burnin+niter))
+#   Sigma[,,1] <- t(Y - X %*% beta[,,1]) %*% (Y - X %*% beta[,,1]) / (nrow(Y)-1)
+#   n <- nrow(X)
+#   p <- ncol(X)
+#   X <- matrix(as.double(X), nrow = n, ncol = p, byrow = FALSE)
+#   AMAT <- cbind(1, A)
+#   XtXgj <- apply(X, 2, FUN = function(x) sum(x^2))
+# 
+#   for(i in 2:(burnin+niter)) {
+#     b[,,i] <- b_update(X = X, Y = Y, Z = Z[,,i-1], beta = beta[,,i-1], bsigma = bsigma[i-1], Sigma = Sigma[,,i-1], XtX = XtXgj)
+#     bsigma[i] <- bsigma_update(X = X, Y = Y, b = b[,,i], Sigma = Sigma[,,i-1])
+#     pi0[i] <- gpi_update(alpha[i-1,])
+#     chi[i,] <- chi_update(gamma = gamma[i-1,], AMAT = AMAT, a = a[i-1,])
+#     a[i,] <- a_update(AMAT = AMAT, chi = chi[i,])
+#     pi1[i,] <- spi_update(AMAT = AMAT, a = a[i,])
+#     pi2[i] <- tpi_update(omega[i-1,])
+#     alpha[i,] <- alpha_update(X = X, Y = Y, b = b[,,i], group = group, pi0 = pi0[i], alpha = alpha[i-1,], gamma = gamma[i-1,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+#     gamma[i,] <- gamma_update(X = X, Y = Y, b = b[,,i], group = group, pi1 = pi1[i,], alpha = alpha[i,], gamma = gamma[i-1,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+#     omega[i,] <- omega_update(X = X, Y = Y, b = b[,,i], group = group, pi2 = pi2[i], alpha = alpha[i,], gamma = gamma[i,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+#     Z[,,i] <- Z_update(X = X, Y = Y, group = group, alpha = alpha[i,], gamma = gamma[i,], omega = omega[i,])
+#     beta[,,i] <- beta_update(b = b[,,i], Z = Z[,,i])
+#     Sigma[,,i] <- Sigma_update(X = X, Y = Y, beta = beta[,,i], b = b[,,i], bsigma = bsigma[i])
+#   }
+#   lreturn <- list(group_pi = pi0, snp_pi = pi1, trait_pi = pi2, group_indicator = alpha,
+#                   snp_indicator = gamma, trait_indicator = omega, b = b, beta = beta,
+#                   chi = chi, probit_a = a, Sigma = Sigma, biter = c(rep(0, burnin), rep(1, niter)),
+#                   burnin = burnin, niter = niter,
+#                   X = X, Y = Y, group = group, bsigma = bsigma, Z = Z)
+#   return(lreturn)
+# }
 
 f <- function(X, Y, group, A, burnin = 2500, niter = 5000) {
   pi0 <- numeric(burnin+niter); pi0[1] <- rbeta(1,1,1)
   pi1 <- matrix(NA, nrow = burnin+niter, ncol = length(group)); pi1[1,] <- rbeta(n = length(group), 1, 1)
   pi2 <- numeric(burnin+niter); pi2[1] <- rbeta(1,1,1)
-  alpha <- matrix(NA, nrow = burnin+niter, ncol = length(unique(group))); alpha[1,] <- rbinom(n = length(unique(group)), size = 1, prob = pi0[1])
-  gamma <- matrix(NA, nrow = burnin+niter, ncol = length(group)); gamma[1,] <- rbinom(n = length(group), size = 1, prob = pi1[1,])
-  omega <- matrix(NA, nrow = burnin+niter, ncol = ncol(X) * ncol(Y)); omega[1,] <- rbinom(n = ncol(X) * ncol(Y), size = 1, prob = pi2[1])
+  alpha <- matrix(NA, nrow = burnin+niter, ncol = length(unique(group))); alpha[1,] <- as.numeric(aggregate(A ~ group, FUN = function(x) sum(x)>0)[,2])#alpha[1,] <- rbinom(n = length(unique(group)), size = 1, prob = pi0[1])
+  gamma <- matrix(NA, nrow = burnin+niter, ncol = length(group)); gamma[1,] <- A
+  omega <- matrix(NA, nrow = burnin+niter, ncol = ncol(X) * ncol(Y)); omega[1,] <- rep(A, length.out = ncol(X) * ncol(Y))
   chi <- matrix(NA, nrow = burnin+niter, ncol = length(group)); chi[1,] <- rnorm(n = length(group))
   a <- matrix(NA, nrow = burnin + niter, ncol = 2); a[1,] <- rnorm(n = 2)
   Z <- array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
@@ -21,32 +69,113 @@ f <- function(X, Y, group, A, burnin = 2500, niter = 5000) {
   beta <-  array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
   beta[,,1] <- Z[,,1] * b[,,1]
   Sigma <- array(NA, dim = c(ncol(Y), ncol(Y), burnin+niter))
-  Sigma[,,1] <- t(Y - X %*% beta[,,1]) %*% (Y - X %*% beta[,,1]) / nrow(Y-1)
+  Sigma[,,1] <- t(Y - X %*% beta[,,1]) %*% (Y - X %*% beta[,,1]) / (nrow(Y)-1)
   n <- nrow(X)
   p <- ncol(X)
   X <- matrix(as.double(X), nrow = n, ncol = p, byrow = FALSE)
   AMAT <- cbind(1, A)
-  XtXgj <- apply(X, 2, FUN = function(x) sum(x^2))
-
+  #XtXgj <- apply(X, 2, FUN = function(x) sum(x^2))
+  XtX <- t(X) %*% X
+  XtY <- t(X) %*% Y
+  
   for(i in 2:(burnin+niter)) {
-    b[,,i] <- b_update(X = X, Y = Y, Z = Z[,,i-1], beta = beta[,,i-1], bsigma = bsigma[i-1], Sigma = Sigma[,,i-1], XtX = XtXgj)
+    b[,,i] <- b_update(X = X, Y = Y, Z = Z[,,i-1], beta = beta[,,i-1], bsigma = bsigma[i-1], Sigma = Sigma[,,i-1], XtX = XtX, XtY = XtY)
     bsigma[i] <- bsigma_update(X = X, Y = Y, b = b[,,i], Sigma = Sigma[,,i-1])
+    #bsigma[i] <- 0.2
     pi0[i] <- gpi_update(alpha[i-1,])
     chi[i,] <- chi_update(gamma = gamma[i-1,], AMAT = AMAT, a = a[i-1,])
     a[i,] <- a_update(AMAT = AMAT, chi = chi[i,])
     pi1[i,] <- spi_update(AMAT = AMAT, a = a[i,])
     pi2[i] <- tpi_update(omega[i-1,])
     alpha[i,] <- alpha_update(X = X, Y = Y, b = b[,,i], group = group, pi0 = pi0[i], alpha = alpha[i-1,], gamma = gamma[i-1,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+    #alpha[i,] <- rep(1, ncol(alpha))
     gamma[i,] <- gamma_update(X = X, Y = Y, b = b[,,i], group = group, pi1 = pi1[i,], alpha = alpha[i,], gamma = gamma[i-1,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+    #gamma[i,] <- gamma[i-1,]
     omega[i,] <- omega_update(X = X, Y = Y, b = b[,,i], group = group, pi2 = pi2[i], alpha = alpha[i,], gamma = gamma[i,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+    #omega[i,] <- omega[i-1,]
     Z[,,i] <- Z_update(X = X, Y = Y, group = group, alpha = alpha[i,], gamma = gamma[i,], omega = omega[i,])
     beta[,,i] <- beta_update(b = b[,,i], Z = Z[,,i])
     Sigma[,,i] <- Sigma_update(X = X, Y = Y, beta = beta[,,i], b = b[,,i], bsigma = bsigma[i])
+    #Sigma[,,i] <- Sigma[,,1]
   }
   lreturn <- list(group_pi = pi0, snp_pi = pi1, trait_pi = pi2, group_indicator = alpha,
                   snp_indicator = gamma, trait_indicator = omega, b = b, beta = beta,
                   chi = chi, probit_a = a, Sigma = Sigma, biter = c(rep(0, burnin), rep(1, niter)),
                   burnin = burnin, niter = niter,
-                  X = X, Y = Y, group = group, bsigma = bsigma, Z = Z)
+                  X = X, Y = Y, group = group, bsigma = bsigma, Z = Z, nupdate = 0, A = A)
+  return(lreturn)
+}
+
+f_extend <- function(output, nupdate) {
+  #pi0 <- numeric(burnin+niter); pi0[1] <- rbeta(1,1,1)
+  burnin <- output$burnin
+  niter <- output$niter
+  burniter <- burnin + niter + output$nupdate
+  group <- output$group
+  A <- output$A
+  Y <- output$Y
+  n <- nrow(output$X)
+  p <- ncol(output$X)
+  X <- matrix(as.double(output$X), nrow = n, ncol = p, byrow = FALSE)
+  pi0 <- numeric(burniter + nupdate); pi0[1:burniter] <- output$group_pi
+  #pi1 <- matrix(NA, nrow = burnin+niter, ncol = length(group)); pi1[1,] <- rbeta(n = length(group), 1, 1)
+  pi1 <- matrix(NA, nrow = burniter + nupdate, ncol = length(output$group)); pi1[1:burniter,] <- output$snp_pi
+  #pi2 <- numeric(burnin+niter); pi2[1] <- rbeta(1,1,1)
+  pi2 <- numeric(burniter + nupdate); pi2[1:burniter] <- output$trait_pi
+  #alpha <- matrix(NA, nrow = burnin+niter, ncol = length(unique(group))); alpha[1,] <- as.numeric(aggregate(A ~ group, FUN = function(x) sum(x)>0)[,2])#alpha[1,] <- rbinom(n = length(unique(group)), size = 1, prob = pi0[1])
+  alpha <- matrix(NA, nrow = burniter + nupdate, ncol = length(unique(output$group))); alpha[1:burniter,] <- output$group_indicator
+  #gamma <- matrix(NA, nrow = burnin+niter, ncol = length(group)); gamma[1,] <- A
+  gamma <- matrix(NA, nrow = burniter + nupdate, ncol = length(output$group)); gamma[1:burniter,] <- output$snp_indicator
+  #omega <- matrix(NA, nrow = burnin+niter, ncol = ncol(X) * ncol(Y)); omega[1,] <- rep(A, length.out = ncol(X) * ncol(Y))
+  omega <- matrix(NA, nrow = burniter + nupdate, ncol = ncol(output$X) * ncol(output$Y)); omega[1:burniter,] <- output$trait_indicator
+  #chi <- matrix(NA, nrow = burnin+niter, ncol = length(group)); chi[1,] <- rnorm(n = length(group))
+  chi <- matrix(NA, nrow = burniter + nupdate, ncol = length(group)); chi[1:burniter,] <- output$chi
+  #a <- matrix(NA, nrow = burnin + niter, ncol = 2); a[1,] <- rnorm(n = 2)
+  a <- matrix(NA, nrow = burniter + nupdate, ncol = 2); a[1:burniter,] <- output$probit_a
+  #Z <- array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
+  #Z[,,1] <- 0
+  #Z[which(A==1),,1] <- 1
+  Z <- array(NA, dim = c(ncol(output$X), ncol(output$Y), burniter + nupdate)); Z[,,1:burniter] <- output$Z
+  #bsigma <- numeric(burnin+niter); bsigma[1] <- runif(1, 0, 1)
+  bsigma <- numeric(burniter + nupdate); bsigma[1:burniter] <- output$bsigma
+  #b <- array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
+  #b[,,1] <- solve(t(X) %*% X) %*% t(X) %*% Y #least squares starting point
+  b <- array(NA, dim = c(ncol(output$X), ncol(output$Y), burniter + nupdate)); b[,,1:burniter] <- output$b
+  #beta <-  array(NA, dim = c(ncol(X), ncol(Y), burnin+niter))
+  #beta[,,1] <- Z[,,1] * b[,,1]
+  beta <- array(NA, dim = c(ncol(output$X), ncol(output$Y), burniter + nupdate)); beta[,,1:burniter] <- output$beta
+  #Sigma <- array(NA, dim = c(ncol(Y), ncol(Y), burnin+niter))
+  #Sigma[,,1] <- t(Y - X %*% beta[,,1]) %*% (Y - X %*% beta[,,1]) / (nrow(Y)-1)
+  Sigma <- array(NA, dim = c(ncol(Y), ncol(Y), burniter + nupdate)); Sigma[,,1:burniter] <- output$Sigma
+  AMAT <- cbind(1, A)
+  #XtXgj <- apply(X, 2, FUN = function(x) sum(x^2))
+  XtX <- t(X) %*% X
+  XtY <- t(X) %*% Y
+  
+  for(i in (burniter+1):(burniter+nupdate)) {
+    b[,,i] <- b_update(X = X, Y = Y, Z = Z[,,i-1], beta = beta[,,i-1], bsigma = bsigma[i-1], Sigma = Sigma[,,i-1], XtX = XtX, XtY = XtY)
+    bsigma[i] <- bsigma_update(X = X, Y = Y, b = b[,,i], Sigma = Sigma[,,i-1])
+    #bsigma[i] <- 0.2
+    pi0[i] <- gpi_update(alpha[i-1,])
+    chi[i,] <- chi_update(gamma = gamma[i-1,], AMAT = AMAT, a = a[i-1,])
+    a[i,] <- a_update(AMAT = AMAT, chi = chi[i,])
+    pi1[i,] <- spi_update(AMAT = AMAT, a = a[i,])
+    pi2[i] <- tpi_update(omega[i-1,])
+    alpha[i,] <- alpha_update(X = X, Y = Y, b = b[,,i], group = group, pi0 = pi0[i], alpha = alpha[i-1,], gamma = gamma[i-1,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+    #alpha[i,] <- rep(1, ncol(alpha))
+    gamma[i,] <- gamma_update(X = X, Y = Y, b = b[,,i], group = group, pi1 = pi1[i,], alpha = alpha[i,], gamma = gamma[i-1,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+    #gamma[i,] <- gamma[i-1,]
+    omega[i,] <- omega_update(X = X, Y = Y, b = b[,,i], group = group, pi2 = pi2[i], alpha = alpha[i,], gamma = gamma[i,], omega = omega[i-1,], Sigma = Sigma[,,i-1])
+    #omega[i,] <- omega[i-1,]
+    Z[,,i] <- Z_update(X = X, Y = Y, group = group, alpha = alpha[i,], gamma = gamma[i,], omega = omega[i,])
+    beta[,,i] <- beta_update(b = b[,,i], Z = Z[,,i])
+    Sigma[,,i] <- Sigma_update(X = X, Y = Y, beta = beta[,,i], b = b[,,i], bsigma = bsigma[i])
+    #Sigma[,,i] <- Sigma[,,1]
+  }
+  lreturn <- list(group_pi = pi0, snp_pi = pi1, trait_pi = pi2, group_indicator = alpha,
+                  snp_indicator = gamma, trait_indicator = omega, b = b, beta = beta,
+                  chi = chi, probit_a = a, Sigma = Sigma, biter = c(rep(0, burnin), rep(1, niter)),
+                  burnin = burnin, niter = niter,
+                  X = X, Y = Y, group = group, bsigma = bsigma, Z = Z, A = A, nupdate = output$nupdate + nupdate)
   return(lreturn)
 }
--- R/updater_functions.R
+++ R/updater_functions.R
@@ -1,18 +1,46 @@
 #updater functions
 
-b_update <- function(X, Y, Z, beta, bsigma, Sigma, XtX) {
-  tmp_b <- matrix(nrow = ncol(X), ncol = ncol(Y))
+# b_update <- function(X, Y, Z, beta, bsigma, Sigma, XtX) {
+#   tmp_b <- matrix(nrow = ncol(X), ncol = ncol(Y))
+#   for(colx in 1:ncol(X)) {
+#     m_gj <- as.numeric(solve(XtX[colx] + 1/bsigma)) * (t(X[,colx]) %*% (Y - eigenMapMatMult(X[,-colx], beta[-colx,])))
+#     m_gj <- Z[colx,] * m_gj
+#     sig_gj <- (Z[colx,] %*% t(Z[colx,])) * (as.numeric(solve(XtX[colx] + (1/bsigma)))) * Sigma + (1 - Z[colx,] %*% t(Z[colx,])) * (bsigma * Sigma)
+#     sig_gj[which(Z[colx,]==1),which(Z[colx,]!=1)] <- sqrt((1/bsigma)  * as.numeric(solve(XtX[colx] + 1/bsigma))) * (bsigma * Sigma)[which(Z[colx,]==1),which(Z[colx,]!=1)]
+#     sig_gj[which(Z[colx,]!=1),which(Z[colx,]==1)] <- sqrt((1/bsigma)  * as.numeric(solve(XtX[colx] + 1/bsigma))) * (bsigma * Sigma)[which(Z[colx,]!=1),which(Z[colx,]==1)]
+#     tmp_b[colx,] <- MASS::mvrnorm(n = 1, mu = m_gj, Sigma = sig_gj)
+#   }
+#   return(tmp_b)
+# }
+mymvn <- function (n = 1, mu = rep(0, k), Sigma) 
+{
+  k <- ncol(Sigma)
+  z <- matrix(rnorm(n * k), n, k) %*% chol(Sigma)
+  x <- mu + z
+  return(x)
+}
+b_update <- function(X, Y, Z, beta, bsigma, Sigma, XtX, XtY) {
+  #tmp_b <- matrix(nrow = ncol(X), ncol = ncol(Y))
+  #bmeans <- 0
+  bsigs <- vector("list", ncol(X))
+  #bsigs <- array(dim = c(ncol(Y), ncol(Y), ncol(X)))
+  bmeans <- c(t(Z) * t(solve(XtX + 1/bsigma * diag(ncol(X))) %*% XtY))
   for(colx in 1:ncol(X)) {
-    m_gj <- as.numeric(solve(XtX[colx] + 1/bsigma)) * (t(X[,colx]) %*% (Y - eigenMapMatMult(X[,-colx], beta[-colx,])))
-    m_gj <- Z[colx,] * m_gj
-    sig_gj <- (Z[colx,] %*% t(Z[colx,])) * (as.numeric(solve(XtX[colx] + (1/bsigma)))) * Sigma + (1 - Z[colx,] %*% t(Z[colx,])) * (bsigma * Sigma)
-    sig_gj[which(Z[colx,]==1),which(Z[colx,]!=1)] <- sqrt((1/bsigma)  * as.numeric(solve(XtX[colx] + 1/bsigma))) * (bsigma * Sigma)[which(Z[colx,]==1),which(Z[colx,]!=1)]
-    sig_gj[which(Z[colx,]!=1),which(Z[colx,]==1)] <- sqrt((1/bsigma)  * as.numeric(solve(XtX[colx] + 1/bsigma))) * (bsigma * Sigma)[which(Z[colx,]!=1),which(Z[colx,]==1)]
-    tmp_b[colx,] <- MASS::mvrnorm(n = 1, mu = m_gj, Sigma = sig_gj)
+    #m_gj <- as.numeric(solve(XtX[colx] + 1/bsigma)) * (t(X[,colx]) %*% (Y - eigenMapMatMult(X[,-colx], beta[-colx,])))
+    #m_gj <- Z[colx,] * m_gj
+    #bsigs <- c(bsigs, m_gj)
+    sig_gj <- (Z[colx,] %*% t(Z[colx,])) * (as.numeric(solve(XtX[colx,colx] + (1/bsigma)))) * Sigma + (1 - Z[colx,] %*% t(Z[colx,])) * (bsigma * Sigma)
+    sig_gj[which(Z[colx,]==1),which(Z[colx,]!=1)] <- sqrt((1/bsigma)  * as.numeric(solve(XtX[colx,colx] + 1/bsigma))) * (bsigma * Sigma)[which(Z[colx,]==1),which(Z[colx,]!=1)]
+    sig_gj[which(Z[colx,]!=1),which(Z[colx,]==1)] <- sqrt((1/bsigma)  * as.numeric(solve(XtX[colx,colx] + 1/bsigma))) * (bsigma * Sigma)[which(Z[colx,]!=1),which(Z[colx,]==1)]
+    bsigs[[colx]] <- sig_gj
+    #tmp_b[colx,] <- MASS::mvrnorm(n = 1, mu = m_gj, Sigma = sig_gj)
   }
-  return(tmp_b)
+  return(matrix(mymvn(n = 1, mu = bmeans, Sigma = as.matrix(do.call("bdiag", bsigs))), nrow = ncol(X), ncol = ncol(Y), byrow = TRUE))
 }
 
+# bsigma_update <- function(X, Y, b, Sigma) {
+#   return(LaplacesDemon::rinvgamma(n = 1, shape = 0.5 * ncol(X) * ncol(Y), scale = 0.5 * pracma::Trace(b %*% solve(Sigma) %*% t(b))))
+# }
 bsigma_update <- function(X, Y, b, Sigma) {
   return(LaplacesDemon::rinvgamma(n = 1, shape = 0.5 * ncol(X) * ncol(Y), scale = 0.5 * pracma::Trace(b %*% solve(Sigma) %*% t(b))))
 }
@@ -39,6 +67,30 @@ tpi_update <- function(omega) {
   return(rbeta(n = 1, shape1 = 1 + sum(omega), shape2 = 1 + sum(1-omega)))
 }
 
+# alpha_update <- function(X, Y, b, group, pi0, alpha, gamma, omega, Sigma) {
+#   gunique <- unique(group)
+#   gmat <- matrix(rep(gamma, ncol(Y)), nrow = ncol(X), ncol(Y), byrow = FALSE)
+#   omat <- matrix(omega, ncol = ncol(Y), byrow = FALSE)
+#   SSigma <- solve(Sigma)
+#   for(g in 1:length(gunique)) {
+#     amat <- matrix(rep(rep(alpha, times = table(group)), ncol(Y)), nrow = ncol(X), ncol = ncol(Y), byrow = FALSE)
+#     gg <- which(group == g)
+#     amatg <- 0 * amat
+#     amatg[gg,] <- 1
+#     amatng <- amat
+#     amatng[gg,] <- 0
+#     Zg <- amatg * gmat * omat
+#     Zng <- amatng * gmat * omat
+#     Yng <- Y - eigenMapMatMult(X, (Zng * b))
+#     Bg <- Zg * b
+#     XB <- eigenMapMatMult(X, Bg)
+#     calc1 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yng - XB, SSigma), t(Yng - XB)))
+#     calc2 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yng, SSigma), t(Yng)))
+#     prob <- pi0 / (pi0 + (1-pi0) * exp(calc2 - calc1))
+#     alpha[g] <- rbinom(1, 1, prob)
+#   }
+#   return(alpha)
+# }
 alpha_update <- function(X, Y, b, group, pi0, alpha, gamma, omega, Sigma) {
   gunique <- unique(group)
   gmat <- matrix(rep(gamma, ncol(Y)), nrow = ncol(X), ncol(Y), byrow = FALSE)
@@ -56,14 +108,36 @@ alpha_update <- function(X, Y, b, group, pi0, alpha, gamma, omega, Sigma) {
     Yng <- Y - eigenMapMatMult(X, (Zng * b))
     Bg <- Zg * b
     XB <- eigenMapMatMult(X, Bg)
-    calc1 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yng - XB, SSigma), t(Yns - XB)))
-    calc2 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yng, SSigma), t(Yng)))
+    calc1 <- (-0.5) * sum(eigenMapMatMult(Yng - XB, SSigma) * (Yng - XB))
+    calc2 <- (-0.5) * sum(eigenMapMatMult(Yng, SSigma) * (Yng))
     prob <- pi0 / (pi0 + (1-pi0) * exp(calc2 - calc1))
     alpha[g] <- rbinom(1, 1, prob)
   }
   return(alpha)
 }
 
+# gamma_update <- function(X, Y, b, group, pi1, alpha, gamma, omega, Sigma) {
+#   amat <- matrix(rep(rep(alpha, times = table(group)), ncol(Y)), nrow = ncol(X), ncol = ncol(Y), byrow = FALSE)
+#   omat <- matrix(omega, ncol = ncol(Y), byrow = FALSE)
+#   SSigma <- solve(Sigma)
+#   for(xcol in 1:ncol(X)) {
+#     gmat <- matrix(rep(gamma, ncol(Y)), nrow = ncol(X), ncol(Y), byrow = FALSE)
+#     gmats <- gmat * 0
+#     gmats[xcol,] <- 1
+#     gmatns <- gmat
+#     gmatns[xcol,] <- 0
+#     Zs <- amat * gmats * omat
+#     Zns <- amat * gmatns * omat
+#     Yns <- Y - eigenMapMatMult(X, (Zns * b))
+#     Bs <- Zs * b
+#     XB <- eigenMapMatMult(X, Bs)
+#     calc1 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yns - XB, SSigma), t(Yns - XB)))
+#     calc2 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yns, SSigma), t(Yns)))
+#     prob <- pi1[xcol] / (pi1[xcol] + (1-pi1[xcol]) * exp(calc2 - calc1))
+#     gamma[xcol] <- rbinom(1, 1, prob)
+#   }
+#   return(gamma)
+# }
 gamma_update <- function(X, Y, b, group, pi1, alpha, gamma, omega, Sigma) {
   amat <- matrix(rep(rep(alpha, times = table(group)), ncol(Y)), nrow = ncol(X), ncol = ncol(Y), byrow = FALSE)
   omat <- matrix(omega, ncol = ncol(Y), byrow = FALSE)
@@ -79,13 +153,40 @@ gamma_update <- function(X, Y, b, group, pi1, alpha, gamma, omega, Sigma) {
     Yns <- Y - eigenMapMatMult(X, (Zns * b))
     Bs <- Zs * b
     XB <- eigenMapMatMult(X, Bs)
-    calc1 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yns - XB, SSigma), t(Yns - XB)))
-    calc2 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Yns, SSigma), t(Yns)))
+    calc1 <- (-0.5) * sum(eigenMapMatMult(Yns - XB, SSigma) * (Yns - XB))
+    calc2 <- (-0.5) * sum(eigenMapMatMult(Yns, SSigma) * (Yns))
     prob <- pi1[xcol] / (pi1[xcol] + (1-pi1[xcol]) * exp(calc2 - calc1))
     gamma[xcol] <- rbinom(1, 1, prob)
   }
   return(gamma)
 }
+
+# omega_update <- function(X, Y, b, group, pi2, alpha, gamma, omega, Sigma) {
+#   amat <- matrix(rep(rep(alpha, times = table(group)), ncol(Y)), nrow = ncol(X), ncol = ncol(Y), byrow = FALSE)
+#   gmat <- matrix(rep(gamma, ncol(Y)), nrow = ncol(X), ncol(Y), byrow = FALSE)
+#   SSigma <- solve(Sigma)
+#   tracker <- 1
+#   for(ycol in 1:ncol(Y)) {
+#     for(xcol in 1:ncol(X)) {
+#       omat <- matrix(omega, ncol = ncol(Y), byrow = FALSE)
+#       omatt <-  0 * omat
+#       omatt[xcol,ycol] <- 1
+#       omatnt <- omat
+#       omatnt[xcol,ycol] <- 0
+#       Zt <- amat * gmat * omatt
+#       Znt <- amat * gmat * omatnt
+#       Ynt <- Y - eigenMapMatMult(X, (Znt * b))
+#       Bt <- Zt * b
+#       XB <- eigenMapMatMult(X, Bt)
+#       calc1 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Ynt - XB, SSigma), t(Ynt - XB)))
+#       calc2 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Ynt, SSigma), t(Ynt)))
+#       prob <- pi2 / (pi2 + (1-pi2) * exp(calc2 - calc1))
+#       omega[tracker] <- rbinom(1,1, prob)
+#       tracker <- tracker + 1
+#     }
+#   }
+#   return(omega)
+# }
 
 omega_update <- function(X, Y, b, group, pi2, alpha, gamma, omega, Sigma) {
   amat <- matrix(rep(rep(alpha, times = table(group)), ncol(Y)), nrow = ncol(X), ncol = ncol(Y), byrow = FALSE)
@@ -104,8 +205,8 @@ omega_update <- function(X, Y, b, group, pi2, alpha, gamma, omega, Sigma) {
       Ynt <- Y - eigenMapMatMult(X, (Znt * b))
       Bt <- Zt * b
       XB <- eigenMapMatMult(X, Bt)
-      calc1 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Ynt - XB, SSigma), t(Ynt - XB)))
-      calc2 <- (-0.5) * pracma::Trace(eigenMapMatMult(eigenMapMatMult(Ynt, SSigma), t(Ynt)))
+      calc1 <- (-0.5) * sum(eigenMapMatMult(Ynt - XB, SSigma) * (Ynt - XB))
+      calc2 <- (-0.5) * sum(eigenMapMatMult(Ynt, SSigma) * Ynt)
       prob <- pi2 / (pi2 + (1-pi2) * exp(calc2 - calc1))
       omega[tracker] <- rbinom(1,1, prob)
       tracker <- tracker + 1
@@ -125,6 +226,10 @@ beta_update <- function(b, Z) {
   return(Z * b)
 }
 
+# Sigma_update <- function(X, Y, beta, b, bsigma) {
+#   return(LaplacesDemon::rinvwishart(nu = ncol(X) + ncol(Y) + nrow(Y), S = diag(ncol(Y)) + t(Y - eigenMapMatMult(X, beta)) %*% (Y - eigenMapMatMult(X, beta)) + (1/bsigma) * eigenMapMatMult(t(b), b)))
+# }
 Sigma_update <- function(X, Y, beta, b, bsigma) {
-  return(LaplacesDemon::rinvwishart(nu = ncol(X) + ncol(Y) + nrow(Y), S = diag(ncol(Y)) + t(Y - eigenMapMatMult(X, beta)) %*% (Y - eigenMapMatMult(X, beta)) + (1/bsigma) * eigenMapMatMult(t(b), b)))
+  return(LaplacesDemon::rinvwishart(nu = ncol(X) + ncol(Y) + nrow(Y), S = diag(ncol(Y)) + eigenMapMatMult(t(Y - eigenMapMatMult(X, beta)), (Y - eigenMapMatMult(X, beta))) + (1/bsigma) * eigenMapMatMult(t(b), b)))
+  #return(cIRT::riwishart(df = ncol(X) + ncol(Y) + nrow(Y), S = diag(ncol(Y)) + t(Y - eigenMapMatMult(X, beta)) %*% (Y - eigenMapMatMult(X, beta)) + (1/bsigma) * eigenMapMatMult(t(b), b)))
 }
