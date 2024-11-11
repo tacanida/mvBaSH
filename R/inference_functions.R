a393897e7d13fc538e6b052d24f274dc1c19f9ed
tacanida <135726976+tacanida@users.noreply.github.com>
2024-11-11 14:06:44 -0500

    Update inference_functions.R
    re implement bfdr function

--- R/inference_functions.R
+++ R/inference_functions.R
@@ -13,3 +13,24 @@ get_overall_pip <- function(mcmc_out) {
   return(rowMeans(apply(beta, 3, FUN = function(x) rowSums(x) != 0)))
 }
 
+bfdr <- function(pips, cutoff, tol = 0.01) {
+  phat <- 1 - pips
+  tseq <- seq(min(phat), 1, by=1e-4)
+  bfdrt <- numeric(length(tseq))
+  counter <- 1
+  for(t in tseq) {
+    dk <- as.numeric(phat < t)
+    bfdrt[counter] <- sum(phat * dk) / sum(dk)
+    counter <- counter + 1
+  }
+  diffs <- abs(cutoff - bfdrt)
+  tchoice <- min(which(diffs == min(diffs, na.rm = TRUE)))
+  fdr_est <- bfdrt[tchoice]
+  to_return <- c(1-tseq[tchoice], fdr_est)
+  names(to_return) <- c("cutoff value", "estimated bfdr")
+  #return(1 - tseq[tchoice])
+  if(diffs[tchoice]>tol) {
+    warning("tolerance exceeded: check results")
+  }
+  return(to_return)
+}
